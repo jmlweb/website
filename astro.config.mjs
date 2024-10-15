79a4a8bf370e1070639e3629335b381f6c9e4a60
José Manuel Lucas <josemanuel@jmlweb.es>
2024-10-15 21:21:13 +0200

    feat: implement languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const { SITE_URL = 'https://jmlweb.es' } = loadEnv(
+  process.env.NODE_ENV ?? 'production',
+  process.cwd(),
+  '',
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: { defaultLocale: 'es', locales: ['es', 'en'] },
+  site: SITE_URL,
+});
